06718a648678825837c0e56e537d7e66281f3048
1024sparrow <1024sparrow@gmail.com>
2018-04-01 14:36:37 +0300

    fixed

--- install.sh
+++ install.sh
@@ -16,8 +16,6 @@ fi
 
 echo -e "${YEL}Распаковываю jdk${NC}"
 tar -xf $JDK_FILE
-#export JAVA_HOME=$(pwd)/jdk-9.0.1
-#export PATH=${JAVA_HOME}/bin:$PATH
 
 echo -e "${YEL}Скачиваю и распаковываю sdk-tools${NC}"
 wget https://dl.google.com/android/repository/sdk-tools-linux-3859397.zip
@@ -51,10 +49,5 @@ unzip platform-tools_r26.0.2-linux.zip -d tools/
 mv tools android-sdk-linux
 
 echo export MYANDROID="$(pwd)" >> ~/.bashrc
-cp $(dirname $0)/generates_desktop_home_files_for_desktop_indicator.sh ./
+cp $(dirname $0)/generate_android_build_script.sh ./
 echo export MYANDROID_GEN="$(pwd)/generates_desktop_home_files_for_desktop_indicator.sh" >> ~/.bashrc
-
-#echo export SDK="$(pwd)/android-sdk-linux" >> ~/.bashrc
-#echo export BUILD_TOOLS="${SDK}/build-tools/27.0.1" >> ~/.bashrc
-#echo export PLATFORM="${SDK}/platforms/android-22" >> ~/.bashrc
-#echo export PATH="$SDK/platform-tools":"$SDK/build-tools/27.0.1":"$SDK":"$PATH" >> ~/.bashrc
