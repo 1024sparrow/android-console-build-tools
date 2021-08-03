9ee662e760dcacecb44dd7d8abddd59f1ff29e80
Boris Vasilyev <1024sparrow@gmail.com>
2021-08-03 11:04:46 +0300

    generating example app from scratch

--- generate_android_build_script.sh
+++ generate_android_build_script.sh
@@ -1,6 +1,69 @@
 #!/bin/bash
 # генератор сборочного скрипта для проекта под Android
-# copyright 2018, Васильев Борис
+# copyright 2018-2021, Васильев Борис (1024sparrow@gmail.com)
+
+function generateNewProjectSources {
+    local appname=$1
+    local apppath=$2
+    local apppathDotes=$(echo $apppath | sed 's/\//./g')
+
+    #echo "apppathDotes: $apppathDotes"
+    #echo "apppath: $apppath"
+    #echo "appname: $appname"
+
+    echo "<?xml version=\"1.0\" encoding=\"utf-8\"?>
+<manifest xmlns:android=\"http://schemas.android.com/apk/res/android\"
+          package=\"${apppathDotes}\"
+          versionCode=\"1\"
+          versionName=\"0.1\">
+    <uses-sdk android:minSdkVersion=\"16\"/>
+    <application android:label=\"Hello\">
+        <activity android:name=\".MainActivity\">
+            <intent-filter>
+                <action android:name=\"android.intent.action.MAIN\"/>
+                <category android:name=\"android.intent.category.LAUNCHER\"/>
+            </intent-filter>
+        </activity>
+    </application>
+</manifest>
+" > AndroidManifest.xml
+
+    mkdir -p res/layout
+    echo "<?xml version=\"1.0\" encoding=\"utf-8\"?>
+<LinearLayout
+    xmlns:android=\"http://schemas.android.com/apk/res/android\"
+    android:layout_width=\"match_parent\"
+    android:layout_height=\"match_parent\"
+    android:gravity=\"center\"
+    android:orientation=\"vertical\">
+
+    <TextView
+        android:layout_width=\"wrap_content\"
+        android:layout_height=\"wrap_content\"
+        android:id=\"@+id/my_text\"/>
+</LinearLayout>
+" > res/layout/activity_main.xml
+
+    mkdir -p java/$apppath
+    pushd java/$apppath
+        echo "package ${apppathDotes};
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TextView text = (TextView)findViewById(R.id.my_text);
+        text.setText(\"Hello, world!\");
+    }
+}" > MainActivity.java
+    popd
+} # function generateNewProjectSources
 
 appname=""
 apppath=""
@@ -30,12 +93,20 @@ else
     echo Название проекта: webandroidapp
     echo Путь проекта: ru/mycompany/webandroidapp
     echo ========================================
-    echo -n "Введите название проекта:"
+    echo -n "Введите название проекта: "
     read appname
-    if [[ -z $appname ]]; then exit 0; fi
-    echo -n "Введите путь проекта:"
+    if [[ -z $appname ]]
+    then
+        exit 0
+    fi
+    echo -n "Введите путь проекта: "
     read apppath
-    if [[ -z $apppath ]]; then echo qq; exit 0; fi
+    if [[ -z $apppath ]]
+    then
+        echo qq
+        exit 0
+    fi
+    generateNewProjectSources $appname $apppath
 fi
 
 echo -n "Название скрипта (build.sh по умолчанию):"
@@ -60,67 +131,58 @@ touch $scriptname
 #export MYANDROID_PATH="$MYANDROID_SDK/platform-tools":"$MYANDROID_SDK/build-tools/27.0.1":"$MYANDROID_SDK":"$PATH" >> ~/.bashrc
 
 #==========================
-echo "#!/bin/bash" > $scriptname
-echo "# скрипт, собирающий проект под Android" >> $scriptname
-echo "# Реализация основана на статье, расположенной по адресу http://www.hanshq.net/command-line-android.html" >> $scriptname
-echo >> $scriptname
-echo "appname=${appname}" >> $scriptname
-echo "apppath=${apppath}" >> $scriptname
-echo >> $scriptname
-echo "export JAVA_HOME=\${MYANDROID}/jdk-9.0.1" >> $scriptname
-echo "export PATH=\${JAVA_HOME}/bin:\$PATH" >> $scriptname
-echo "export SDK=\"\${MYANDROID}/android-sdk-linux\"" >> $scriptname
-echo "export BUILD_TOOLS=\"\${SDK}/build-tools/27.0.1\"" >> $scriptname
-echo "export PLATFORM=\"\${SDK}/platforms/android-22\"" >> $scriptname
-echo "export PATH=\"\$SDK/platform-tools\":\"\$SDK/build-tools/27.0.1\":\"\$SDK\":\"\$PATH\"" >> $scriptname
-echo >> $scriptname
-echo "RED='\033[0;31m'" >> $scriptname
-echo "YEL='\033[1;33m'" >> $scriptname
-echo "NC='\033[0m'" >> $scriptname
-echo >> $scriptname
+echo "#!/bin/bash
+# скрипт, собирающий проект под Android
+# Реализация основана на статье, расположенной по адресу http://www.hanshq.net/command-line-android.html
 
-echo "function error_quite {" >> $scriptname
-echo "    echo -e \"\${RED}Сборка прервана из-за возникшей ошибки\${NC}\"" >> $scriptname
-echo "    exit 1" >> $scriptname
-echo "}" >> $scriptname
-echo >> $scriptname
+appname=${appname}
+apppath=${apppath}
 
-echo "echo -e \"\${YEL}Удаляем build (предыдущую сборку)\${NC}\"" >> $scriptname
-echo "rm -rf build" >> $scriptname
-echo >> $scriptname
+export JAVA_HOME=\${MYANDROID}/jdk-9.0.1
+export PATH=\${JAVA_HOME}/bin:\$PATH
+export SDK=\"\${MYANDROID}/android-sdk-linux\"
+export BUILD_TOOLS=\"\${SDK}/build-tools/27.0.1\"
+export PLATFORM=\"\${SDK}/platforms/android-22\"
+export PATH=\"\$SDK/platform-tools\":\"\$SDK/build-tools/27.0.1\":\"\$SDK\":\"\$PATH\"
 
-echo "echo -e \"\${YEL}Создаём директории, куда будет производиться сборка\${NC}\"" >> $scriptname
-echo "mkdir -p build/gen build/obj build/apk || error_quite" >> $scriptname
-echo >> $scriptname
+RED='\033[0;31m'
+YEL='\033[1;33m'
+NC='\033[0m'
 
-echo "echo -e \"\${YEL}Генерируем java-файлы (R.java), отображающие содержимое ресурсов (которые описаны в XML)\${NC}\"" >> $scriptname
-echo "\"\${BUILD_TOOLS}/aapt\" package -f -m -J build/gen/ -S res -M AndroidManifest.xml -I \"\${PLATFORM}/android.jar\" || error_quite" >> $scriptname
-echo >> $scriptname
+function error_quite {
+    echo -e \"\${RED}Сборка прервана из-за возникшей ошибки\${NC}\"
+    exit 1
+}
 
-echo "echo -e \"\${YEL}Собираем байт-код для нашего Java-приложения. Делаем байт-код для версии Java 7.\${NC}\"" >> $scriptname
-echo "javac -source 1.7 -target 1.7 -bootclasspath \"\${JAVA_HOME}/jre/lib/rt.jar\" -classpath \"\${PLATFORM}/android.jar\" -d build/obj build/gen/\${apppath}/R.java java/\${apppath}/MainActivity.java || error_quite" >> $scriptname
-echo "echo \"Вот мы получили байт-код Java (файлы .class). Но Android использует байт-код другого формата - Dalvik (файлы .dex)\"" >> $scriptname
-echo >> $scriptname
+echo -e \"\${YEL}Удаляем build (предыдущую сборку)\${NC}\"
+rm -rf build
 
-echo "echo -e \"\${YEL}Преобразуем стандартный байт-код в Андроидовский (Dalvik) байт-код\${NC}\"" >> $scriptname
-echo "\"\${BUILD_TOOLS}/dx\" --dex --output=build/apk/classes.dex build/obj/ || error_quite" >> $scriptname
-echo >> $scriptname
+echo -e \"\${YEL}Создаём директории, куда будет производиться сборка\${NC}\"
+mkdir -p build/gen build/obj build/apk || error_quite
 
-echo "echo -e \"\${YEL}Запаковываем .dex-файлы, манифест и ресурсы в APK\${NC}\"" >> $scriptname
-echo "\"\${BUILD_TOOLS}/aapt\" package -f -M AndroidManifest.xml -A assets -S res/ -I \"\${PLATFORM}/android.jar\" -F build/\${appname}.unsigned.apk build/apk/ || error_quite" >> $scriptname
-echo "echo \"У нас есть apk-файл, но прежде чем его устанавливать на смартфон, его необходимо подписать...\"" >> $scriptname
-echo >> $scriptname
+echo -e \"\${YEL}Генерируем java-файлы (R.java), отображающие содержимое ресурсов (которые описаны в XML)\${NC}\"
+\"\${BUILD_TOOLS}/aapt\" package -f -m -J build/gen/ -S res -M AndroidManifest.xml -I \"\${PLATFORM}/android.jar\" || error_quite
 
-echo "echo -e \"\${YEL}Делаем так, чтобы после распаковки нашего apk файлы были выровнены по размеру блока 4 байта\${NC}\"" >> $scriptname
-echo "\"\${BUILD_TOOLS}/zipalign\" -f -p 4 build/\${appname}.unsigned.apk build/\${appname}.aligned.apk || error_quite" >> $scriptname
-echo >> $scriptname
+echo -e \"\${YEL}Собираем байт-код для нашего Java-приложения. Делаем байт-код для версии Java 7.\${NC}\"
+javac -source 1.7 -target 1.7 -bootclasspath \"\${JAVA_HOME}/jre/lib/rt.jar\" -classpath \"\${PLATFORM}/android.jar\" -d build/obj build/gen/\${apppath}/R.java java/\${apppath}/MainActivity.java || error_quite
+echo \"Вот мы получили байт-код Java (файлы .class). Но Android использует байт-код другого формата - Dalvik (файлы .dex)\"
 
-echo "echo -e \"\${YEL}Генерируем ключ (будут запрашиваться данные у пользователя)\${NC}\"" >> $scriptname
-echo "keytool -genkeypair -keystore keystore.jks -alias androidkey -validity 10000 -keyalg RSA -keysize 2048 -storepass android -keypass android || error_quite" >> $scriptname
-echo >> $scriptname
+echo -e \"\${YEL}Преобразуем стандартный байт-код в Андроидовский (Dalvik) байт-код\${NC}\"
+\"\${BUILD_TOOLS}/dx\" --dex --output=build/apk/classes.dex build/obj/ || error_quite
 
-echo "echo -e \"\${YEL}Подписываем полученным ключом наш apk\${NC}\"" >> $scriptname
-echo "\"\${BUILD_TOOLS}/apksigner\" sign --ks keystore.jks --ks-key-alias androidkey --ks-pass pass:android --key-pass pass:android --out build/\${appname}.apk build/\${appname}.aligned.apk || error_quite" >> $scriptname
-echo >> $scriptname
+echo -e \"\${YEL}Запаковываем .dex-файлы, манифест и ресурсы в APK\${NC}\"
+#\"\${BUILD_TOOLS}/aapt\" package -f -M AndroidManifest.xml -A assets -S res/ -I \"\${PLATFORM}/android.jar\" -F build/\${appname}.unsigned.apk build/apk/ || error_quite
+\"\${BUILD_TOOLS}/aapt\" package -f -M AndroidManifest.xml -S res/ -I \"\${PLATFORM}/android.jar\" -F build/\${appname}.unsigned.apk build/apk/ || error_quite
+echo \"У нас есть apk-файл, но прежде чем его устанавливать на смартфон, его необходимо подписать...\"
+
+echo -e \"\${YEL}Делаем так, чтобы после распаковки нашего apk файлы были выровнены по размеру блока 4 байта\${NC}\"
+\"\${BUILD_TOOLS}/zipalign\" -f -p 4 build/\${appname}.unsigned.apk build/\${appname}.aligned.apk || error_quite
+
+echo -e \"\${YEL}Генерируем ключ (будут запрашиваться данные у пользователя)\${NC}\"
+keytool -genkeypair -keystore keystore.jks -alias androidkey -validity 10000 -keyalg RSA -keysize 2048 -storepass android -keypass android || error_quite
+
+echo -e \"\${YEL}Подписываем полученным ключом наш apk\${NC}\"
+\"\${BUILD_TOOLS}/apksigner\" sign --ks keystore.jks --ks-key-alias androidkey --ks-pass pass:android --key-pass pass:android --out build/\${appname}.apk build/\${appname}.aligned.apk || error_quite
+" > $scriptname
 
 chmod +x $scriptname
